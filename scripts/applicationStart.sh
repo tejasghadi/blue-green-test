57e9b2d4f31e9ebbc0618426a5b31f3590d716b4
Archana <archanab1894@gmail.com>
2020-03-02 20:57:34 +0530

    Add comments for deployment script

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,9 +1,14 @@
 cd /var/www/apps/hello-world
 
+
+ # Pull the environment variables stored in parameter store and write it to a .env file
+
 aws ssm get-parameters-by-path --region us-west-2 --path /hello-world/production/ --with-decryption \
   --query "Parameters[*].{Name:Name,Value:Value}" | jq -c '.[]' \
   |  while read i; do  \
   key=$(echo $i | jq '.Name'| sed -e 's/^"//' -e 's/"$//')  \
   value=$(echo $i | jq '.Value'| sed -e 's/^"//' -e 's/"$//'); echo $key=$value >> .env; done
+
+# Starting the server...
 
 bash -lc "bundle exec rails server -d -p 80 -b 0.0.0.0"
